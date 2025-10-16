2088deed7ec000152e66df9984b2243da005238c
GuilhermeAugusto <guizinaugusto2001@gmail.com>
2025-10-15 21:07:21 -0300

    poof

--- Aula22/mini-licao/licao.js
+++ Aula22/mini-licao/licao.js
@@ -1,7 +1,10 @@
 function questao01(){
+    let resposta = document.querySelector("#resposta01");
+    console.log(resposta.textContent);
     let n = 0;
     while(n < 500){
-        document.write(`${n} `);
+        console.log(n);
+        resposta.textContent += `${n}`;
         n += 1;
     }
 }
@@ -11,7 +14,9 @@ questao01();
 
 function questao02(){
     let A = 0;
-    let B = 30;
+    let resposta = document.querySelector("#resposta02");
+    let val01 = (document.querySelector("valor01").value);
+    let val02 = (document.querySelector("valor02").value);
     for (let i = A; i <= 30; i++){
         if(i % 2 == 0){
             document.write(`${i} `);
@@ -20,6 +25,14 @@ function questao02(){
 }
 
 questao02();
+
+function questao03(){
+    let saida = document.querySelector("#resposta03");
+    let entrada = parseInt(document.querySelector("#entrada").value);
+    for(let i = 0; i <= 10; i++){
+        saida.textContent += `${entrada * i}\n`;
+    }
+}
 
 
     
